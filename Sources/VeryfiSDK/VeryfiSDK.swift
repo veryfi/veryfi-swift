0a0a57380e3cbb91cd43d153e7b734ea69cf872f
Sebastian Carmona <sebastiancarmona@hotmail.ca>
2021-11-21 21:07:46 -0500

    Add doc

--- Sources/VeryfiSDK/VeryfiSDK.swift
+++ Sources/VeryfiSDK/VeryfiSDK.swift
@@ -13,6 +13,13 @@ struct VeryfiCredentials {
 
 class Client: NetworkManager {
     
+    /// Init Client.
+    /// - Parameters:
+    ///   - clientId: Your client id from veryfi-hub.
+    ///   - clientSecret: Your client secret from veryfi-hub.
+    ///   - username: Your username from veryfi-hub.
+    ///   - apiKey: Your api key from veryfi-hub.
+    ///   - apiVersion: Api version to use, by default "v8".
     init(clientId: String, clientSecret: String, username: String, apiKey: String, apiVersion: String = "v8") {
         let credentials = VeryfiCredentials(clientId: clientId, clientSecret: clientSecret, username: username, apiKey: apiKey)
         super.init(credentials: credentials, apiVersion: apiVersion)
